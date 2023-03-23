9f0059507d37461e1890a4b9812cdb79983d2c3e
Ekaterina <c.hirko.va@hotmail.com>
2023-03-23 11:30:43 +0300

    almost done

--- Задача1/Program.cs
+++ Задача1/Program.cs
@@ -2,54 +2,44 @@
 // длинна которых меньше либо равна 3 символа.
 
 
-string[] array = new string[3] {"913", "window", "Street"};
 
-for(int i = 0; i < array.Length; i++)
+string[] Array = new string[5] {"window", "89", "sun", "846352", "764"};
+
+string[] CountAndResultArray(string[] Array, int countOfSymbols) 
 {
-    if(i > )
+    int[] itemNumDegreeId = new int[Array.Length];
+    int countOfItems = 0;
+    for(int i = 0; i < Array.Length; i++)
+    {
+        if(Array[i].Length <= countOfSymbols)
+        {
+            itemNumDegreeId[countOfItems] = i;
+            countOfItems++;
+        }
+    }
+    string[] resultArray = new string[countOfItems];
+    for(int i = 0; i < countOfItems; i++)
+    {
+        resultArray[i] = Array[itemNumDegreeId[i]];
+    }
+    return resultArray;
 }
 
 
 
-// string[] array = new string[3] {"12354", "432", "as"};
 
-// string[] ArrayItemNumDegree(string[] array, int countOfDigits)
-// {
-//     int[] itemNumDegreeId = new int[array.Length];
-//     int countOfItems = 0;
-//     for(int i = 0; i < array.Length; i++)
-//     {
-//         if(array[i].Length <= countOfDigits)
-//         {
-//             itemNumDegreeId[countOfItems] = i;
-//             countOfItems++;
-//         }
-//     }
-//     string[] resultArray = new string[countOfItems];
-//     for(int i = 0; i < countOfItems; i++)
-//     {
-//         resultArray[i] = array[itemNumDegreeId[i]];
-//     }
-//     return resultArray;
-// }
+Console.WriteLine($"[{string.Join(",",Array)}]->[{string.Join(",", CountAndResultArray(Array, 3))}]");
+Console.WriteLine($"[{string.Join(",",Array)}]->[{string.Join(",", CountAndResultArray(Array, 2))}]");
+Console.WriteLine($"[{string.Join(",",Array)}]->[{string.Join(",", CountAndResultArray(Array, 1))}]");
 
+// string str = "abcdefg";
+// Console.WriteLine("The length of '{0}' is {1}", str, str.Length);
+// Console.WriteLine("The length of '{0}' is {1}", "xyz", "xyz".Length);
 
-// Console.WriteLine($"[{string.Join(",",array)}]->[{string.Join(",", ArrayItemNumDegreeLinq(array, 3))}]");
-// Console.WriteLine($"[{string.Join(",",array)}]->[{string.Join(",", ArrayItemNumDegreeLinq(array, 2))}]");
-// Console.WriteLine($"[{string.Join(",",array)}]->[{string.Join(",", ArrayItemNumDegreeLinq(array, 1))}]");
+// int length = str.Length;
+// Console.WriteLine("The length of '{0}' is {1}", str, length);
 
-// Console.WriteLine($"[{string.Join(",",array)}]->[{string.Join(",", ArrayItemNumDegree(array, 3))}]");
-// Console.WriteLine($"[{string.Join(",",array)}]->[{string.Join(",", ArrayItemNumDegree(array, 2))}]");
-// Console.WriteLine($"[{string.Join(",",array)}]->[{string.Join(",", ArrayItemNumDegree(array, 1))}]");
-
-string str = "abcdefg";
-Console.WriteLine("The length of '{0}' is {1}", str, str.Length);
-Console.WriteLine("The length of '{0}' is {1}", "xyz", "xyz".Length);
-
-int length = str.Length;
-Console.WriteLine("The length of '{0}' is {1}", str, length);
-
-// This example displays the following output:
-//    1) The length of 'abcdefg' is 7
-//    2) The length of 'xyz' is 3
-//    3) The length of 'abcdefg' is 7
+// // This example displays the following output:
+// //    1) The length of 'abcdefg' is 7
+// //    2) The length of 'xyz' is 3
+// //    3) The length of 'abcdefg' is 7
