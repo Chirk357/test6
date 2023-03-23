260ab55a7405d84ac0f04fa77ffa9a913d361d86
Ekaterina <c.hirko.va@hotmail.com>
2023-03-23 20:26:41 +0300

    comments finalized

--- Задача1/Program.cs
+++ Задача1/Program.cs
@@ -7,7 +7,7 @@ string[] Array = new string[5] {"window", "89", "1", "846352", "764"}; // Зад
 
 string[] CountAndResultArray(string[] Array, int countOfSymbols) // Созадем метод, который будет считать кол-во символов в слове и выдаст результирующий массив строк
 {
-    int[] defineQuantityOfSymbols = new int[Array.Length]; // создаем массив целых чисел с переменной count и циклом для подсчета кол-ва символом в слове
+    int[] defineQuantityOfSymbols = new int[Array.Length]; // создаем массив целых чисел с циклом для подсчета кол-ва символом в слове и запоминаем слова с нужным кол-м символов
     
     int count = 0; 
     for(int i = 0; i < Array.Length; i++)
@@ -19,7 +19,7 @@ string[] CountAndResultArray(string[] Array, int countOfSymbols) // Созаде
         }
     }
 
-    string[] newArray = new string[count]; // Создаем результирующий массив строк с, используя результат предыдущего блока кода
+    string[] newArray = new string[count]; // Создаем результирующий массив строк, используя результат предыдущего блока кода
     for(int i = 0; i < count; i++)
     {
         newArray[i] = Array[defineQuantityOfSymbols[i]];
@@ -28,13 +28,13 @@ string[] CountAndResultArray(string[] Array, int countOfSymbols) // Созаде
 }
 
 
-string[] Array2 = new string[5] {"1", "Ok", "timeless", "93820183746", "run"};
-Console.WriteLine($"[{string.Join(", ", Array2)}] -> [{string.Join(", ", CountAndResultArray(Array2, 3))}]");
+string[] Array2 = new string[5] {"1", "Ok", "timeless", "93820183746", "run"}; 
+Console.WriteLine($"[{string.Join(", ", Array2)}] -> [{string.Join(", ", CountAndResultArray(Array2, 3))}]"); // вызываем метод и задаем кол-во симоволов для слова =3.
 
 string[] Array3 = new string[5] {"Divan", "vaza", "93", "Ika", "uha"};
-Console.WriteLine($"[{string.Join(", ", Array3)}] -> [{string.Join(", ", CountAndResultArray(Array3, 2))}]");
+Console.WriteLine($"[{string.Join(", ", Array3)}] -> [{string.Join(", ", CountAndResultArray(Array3, 2))}]"); // вызываем метод и задаем кол-во симоволов для слова =2.
 
 string[] Array4 = new string[3] {"7", "5", "juke"};
-Console.WriteLine($"[{string.Join(", ", Array4)}] -> [{string.Join(", ", CountAndResultArray(Array4, 1))}]");
+Console.WriteLine($"[{string.Join(", ", Array4)}] -> [{string.Join(", ", CountAndResultArray(Array4, 1))}]"); // вызываем метод и задаем кол-во симоволов для слова =1.
 
 
